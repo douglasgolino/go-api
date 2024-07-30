a76f4e9da258fe01608522e48cd8e9d6f1594963
Douglas Aguiar <douglas.aguiar@mfrural.com.br>
2024-07-30 13:45:36 -0300

    Script do banco de dados

---
+++ script.sql
@@ -0,0 +1,9 @@
+create table product(
+	id Serial primary key,
+	product_name varchar(50) not null,
+	price numeric (10,2) not null
+);
+
+insert into product (product_name, price) values('Sushi', 100);
+insert into product (product_name, price) values('Tamaki', 20);
+
